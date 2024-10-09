3f6603db961a1a5641e1297903c0256da5a43467
Kathia Barahona <kathiabarahona96@gmail.com>
2024-10-09 18:18:56 +0200

    Structure for BaseClient class (GET/POST) Created a base class that handles get and psot requests, such class must be inherited whenever a new API is needed to be implemented

--- velodyssey/velodyssey/clients/base_client.py
+++ velodyssey/velodyssey/clients/base_client.py
@@ -6,6 +6,7 @@ from requests.exceptions import RequestException
 
 class BaseClient:
     API_BASE_URL = ""
+    TIMEOUT = 60
 
     def __init__(self, logging: Logger):
         self.session = requests.Session()
@@ -14,16 +15,27 @@ class BaseClient:
     def _get_headers(self) -> dict[str, str]:
         raise NotImplementedError
 
-    def get(self, path: str, params: dict = None) -> requests.Response:
+    def get(self, path: str, params: dict | None = None) -> requests.Response:
         headers = self._get_headers()
         url = f"{self.API_BASE_URL}{path}"
+
         try:
-            response = self.session.get(url, params=params, headers=headers)
+            response = self.session.get(
+                url, params=params, headers=headers, timeout=self.TIMEOUT,
+            )
             response.raise_for_status()
             return response
         except RequestException:
             self.logging.error(f"Failed when requesting {url}.")
             raise
 
-    def post(self):
-        ...
+    def post(self, path: str, data: dict | None = None) -> requests.Response:
+        url = f"{self.API_BASE_URL}{path}"
+        headers = self._get_headers()
+        try:
+            response = self.session.post(url, data=data, headers=headers)
+            response.raise_for_status()
+            return response
+        except RequestException as e:
+            self.logging.error(f"Error during POST request: {e}")
+            raise
